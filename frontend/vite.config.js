9c6679095a5cb829babb4654f8c8e65922d04904
Shubham Yadav <mysquad0007@gmail.com>
2026-04-12 20:04:57 +0530

    mobile done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-=======
   define: {
     'process.env': {},
   },
@@ -17,7 +11,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
->>>>>>> dev
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
